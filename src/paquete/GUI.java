583a991c9d754daeea94be4c189e4d738791e0a5
alejandraquimi <alevquim@espol.edu.ec>
2021-06-19 12:58:31 -0500

    cambiar color de pantalla

--- src/paquete/GUI.java
+++ src/paquete/GUI.java
@@ -1,17 +1,33 @@
 package paquete;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GUI extends JFrame {
-	JButton boton1,boton2,boton3;
-	 GUI(){
+	public JButton boton1,boton2,boton3;
+	public FlowLayout fl;
+	
+
+	
+	public GUI(){
 		super("Ventana");
 		setBounds(200,200,200,200);
+		setSize(500,500);
 		setVisible(true);
-		setLayout(new FlowLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		vista();
+		
+	}
+	public void vista() {
+		fl=new FlowLayout();
+		setLayout(fl);
+		
 		boton1=new JButton("boton1");
 		boton2=new JButton("boton2");
 		boton3=new JButton("boton3");
@@ -19,15 +35,45 @@ public class GUI extends JFrame {
 		add(boton2);
 		add(boton3);
 		
+		cambiarColor(this,boton1,boton2,boton3);
+		
 	}
 	
+	 public static void cambiarColor(JFrame p,JButton b1,JButton b2,JButton b3) {
+		 b1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				p.getContentPane().setBackground(Color.RED);
+				
+			}
+		});
+		 
+		 b2.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					p.getContentPane().setBackground(Color.CYAN);
+					
+				}
+			});
+		 b3.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					p.getContentPane().setBackground(Color.blue);
+					
+				}
+			});
 
-	public static void main(String[] args) {
-		GUI ventana=new GUI();
-		ventana.setSize(500, 500);
-		
-		
-	}
+		 
+		 
+	 }
+	
+	
+	
+	
+
 	
 	
   
---
+++ src/paquete/principal.java
@@ -0,0 +1,14 @@
+package paquete;
+
+
+public class principal {
+
+
+	public static void main(String[] args) {
+		GUI ventana=new GUI();
+		ventana.setVisible(true);
+	
+		
+		
+	}
+}
